fc7f49578777774955476a764530b4491cce673d
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-05-01 11:14:26 +0530

    Question update

--- JS/dsa.js
+++ JS/dsa.js
@@ -1,7 +1,11 @@
 
-console.log("*\n**\n***\n****\n*****");
+// console.log("*\n**\n***\n****\n*****");
 
-console.log("*****\n   * \n  *  \n *   \n*****");
+
+
+
+
+// console.log("*****\n   * \n  *  \n *   \n*****");
 
 
 //Data:-- store(var)-->In Ram memory --> process(x+y,add,remove,delete,) --> display;
@@ -15,22 +19,22 @@ var sum = x + y;
 // xy(product):--
 
 
-console.log(x*y);
-console.log((x*y)/(x+y));
+// console.log(x*y);
+// console.log((x*y)/(x+y));
 
 // condtion statment:---
 // ------
 // ------
 // ------
 if(x%2 ==0){
-    console.log("Even");
+    // console.log("Even");
 }else{
-    console.log("Odd");
+    // console.log("Odd");
 }
 // -------
 // -------
 // -------
-console.log(" smart Work is better then hard work ");
+// console.log(" smart Work is better then hard work ");
 
 
 
@@ -40,23 +44,23 @@ var a = 14;
 var b = 11;
 
 if(a == b){
-    console.log(`${a} is equal to ${b}`);
+    // console.log(`${a} is equal to ${b}`);
 }else{
     if(a>b){
-        console.log(`${a} is Greater than  ${b}`);
+        // console.log(`${a} is Greater than  ${b}`);
         
     }else{
-        console.log(`${a} is Smaller than  ${b}`);
+        // console.log(`${a} is Smaller than  ${b}`);
 
     }
 }
 if(a == b){
-    console.log(`${a} is equal to ${b}`);
+    // console.log(`${a} is equal to ${b}`);
 }else if(a>b){
     
-        console.log(`${a} is Greater than  ${b}`);  
+        // console.log(`${a} is Greater than  ${b}`);  
 }else{
-    console.log(`${a} is Smaller than  ${b}`);
+    // console.log(`${a} is Smaller than  ${b}`);
 }
 
 
@@ -66,15 +70,15 @@ if(a == b){
 var mark = 50;
 
 if(mark >90){
-    console.log("excellent boy paisa whasool");
+    // console.log("excellent boy paisa whasool");
 }else if(mark > 80 ){
-    console.log("good");
+    // console.log("good");
 }else if(mark > 70 ){
-    console.log("fair");
+    // console.log("fair");
 }else if(mark > 60 ){
-    console.log("meets expections thikkk a");
+    // console.log("meets expections thikkk a");
 }else{
-    console.log("leave the school don't waste your money ,go doing some business 😎");
+    // console.log("leave the school don't waste your money ,go doing some business 😎");
 }
 
 // Loops:----(Repeating Task)
@@ -84,7 +88,7 @@ let i = 0//initialization
 //evaluation
 while(i <= 9){
 
-    console.log(i);
+    // console.log(i);
 
     // Loop ke under kuch likhne pr woh stop hota h.
 
@@ -93,14 +97,14 @@ while(i <= 9){
     i++;//increment:-nahi toh condition false nahi hogi,or maybe infinite hogi.
 }
 
-console.log("Done");
+// console.log("Done");
 
 for(let i=1;i<=9;i++){
 
-console.log(i);
+// console.log(i);
 
 }
-console.log("Done");
+// console.log("Done");
 
 
 // Take Input from user:---
@@ -111,12 +115,12 @@ var user = "sarfraj"
 var userInput = 10;
  var number = Number(userInput)
 
- console.log(`Dear ${user}. Here is the counting `);
+//  console.log(`Dear ${user}. Here is the counting `);
 
  for(let i = 1;i<=number;i++){
-    console.log(i);
+    // console.log(i);
  }
- console.log("end counting");
+//  console.log("end counting");
 
 
 //  Question2 :-- is-Prime Number(1,self)
@@ -169,15 +173,15 @@ for(let i=0;i < numberT; i++){
         }
     }
     if(count ==0){
-        console.log("Prime");
+        // console.log("Prime");
     }else{
-        console.log("not Prime");
+        // console.log("not Prime");
     }
     
 }
 var b = performance.now();
 
-console.log((b-a)/1000);
+// console.log((b-a)/1000);
 
 //Question 3 Print all Prime:-----
 
@@ -198,7 +202,7 @@ for(let i=low;i<=high;i++){
         }
     }
     if(count == 0){
-        console.log(`prime number ${i}`);
+        // console.log(`prime number ${i}`);
     }
 }
 
@@ -225,7 +229,7 @@ for(let i=0;i<tNum;i++){
     a=b;
     b=c;
 }
-console.log(arr);
+// console.log(arr);
 
 // Question 5:--- Count Digits of number that user give  and print
 
@@ -241,28 +245,229 @@ while(num2/10 >=1){
     num2 /= 10;
     // console.log(num2);
     digit++
+   
 }
 
-console.log(`the Digit of the number is ${digit}`);
+// console.log(`the Digit of the number is ${digit}`);
 
 //  Question 6:---- Print number of digit
 
-var num4 = Number(prompt('Number'));
+// var num4 = Number(prompt('Number'));
+var num4 = 100;
 var tp = num4
-var nod = 0;
-var temp = num4;
+var numberOfDigit = 0;
 
-while(temp >=1){
-    temp = temp/10;
-    nod++
+while(tp >= 1){
+    tp = tp/10;
+    numberOfDigit++
 }
 
-console.log(`${num4} digit is`);
-var div = (Math.pow(10,nod-1));
-while( div >=1){
-    var q = Math.floor(num4/div);
-    console.log(q);
+// make divisior
+var div = parseInt(Math.pow(10,numberOfDigit-1));
 
+while(div >=1 ){
+    // quetient
+    var q = parseInt(num4/div);
+
+    // console.log(q);
+    // number reduce to reminder
     num4 = num4%div;
-    div = div/10
+    // reduce divisor
+    div = parseInt(div/10)
 }
+
+// Question 7:---- Reverse A Number 
+// display output in reverse order and line-wise
+
+// var takNum = Number(prompt('Enter the number'));
+var takNum = 123456;
+while(takNum >0){
+    // reminder(last digit)
+    var dig = takNum%10;
+    // quteint(digit before point)
+    takNum = parseInt(takNum/10);
+    // console.log(dig);
+}
+
+// Question 8:--- inverse of a number
+
+// var input = Number(prompt('inversing number'));
+var input = 1000;
+
+var tmp = input;
+
+// inverted number
+let inv = 0;
+// original position
+let op =1;
+
+// run untill the input become 0;
+while( input > 0){
+    // orginal digit(reminder)
+    let od = input % 10;
+    // inverted - digit
+    let id = op;
+    // inverted - position
+    let ip = od;
+    // make change to inv using ip and id
+
+    inv +=id * parseInt(Math.pow(10,ip-1));
+
+    // reduce the input so next qutoent will come
+    input = parseInt(input/10);
+    // increase original position
+    op++
+
+
+}
+
+console.log(`inverse of the number ${tmp} is ${inv}`);
+
+//  Question 9  Rotate a number:--
+// ---> 
+
+// number that need to rotate
+var n = 1234;
+// jitne se rotate karna ha
+var  k = -1;
+
+// find number of digit
+
+var temp = n;
+var nod1 = 0;
+
+while(temp >0){
+    temp = parseInt(temp /10);
+    nod1++
+}
+// if number is larger then number of digit,use  modulus for
+k = k%nod1;
+
+// for negative number 
+if(k < 0){
+    k = k + nod1
+    // console.log(k);
+}
+
+
+// need divisor and multiply for reminder and quetiont 
+var divde = 1;
+var multi = 1;
+// loop 1 se no. of digit tak chalega
+for(let i=1;i<=nod1;i++){
+
+    if(i <= k){
+        // jab tak i chota rehta h k se tab tak divisior ma 10 multiply krte rahe
+        divde = divde * 10;
+    }else{
+        // agar k se bada ho toh mutlipay ma multiply kjiye 10 ko
+        multi = multi * 10;
+     
+    }
+}
+// quotient
+var q = parseInt(n / divde);
+// reminder
+var r =  n % divde;
+
+// reminder multiply by multipler and add quotient
+var result = r * multi + q;
+console.log(result);
+
+// Question 10:----GCD and LCM
+
+// divisor
+// var n1 = Number(prompt('Number 1'))
+var n1 = 36;
+
+// divident
+// var n2 = Number(prompt('Number 2'))
+var n2 = 12;
+
+// if not zero
+if(n1 !== 0 && n2 !== 0){
+    var on1  = n1;
+var on2  = n2;
+
+// loop until n1 % n2 is not equal to 0;
+
+while( n1 % n2 != 0){
+    const reminder = n1 % n2;
+    // ab reminder divisor banega
+    n2 = reminder;//12
+    n1 = n2 //24
+    // ab disvisor divdient banega
+}
+
+const gcd = n2;
+
+// find LCM:---
+
+const lcm = (on1 * on2)/gcd;
+console.log(lcm);
+console.log(` Here GCD :- ${gcd} and LCM :- ${lcm} of the ${on1} and ${on2}`);
+
+}else{
+    console.error('Please enter the valid number');
+}
+
+// Question 11:-Prime Factorization
+
+// var number2 = Number(prompt('enter that number '));
+var number2 = 1440
+var arr = []
+// least number se start krenge and up to root n tak hi chalenge
+
+for(let div = 2; div * div <= number2 ; div++){
+    // ab us number ko divde krenge div tab tak jab tak woh reminder zero de rah ha
+   
+    while(number2 % div === 0 ){
+        number2 = parseInt(number2/div);
+        arr.push(div);
+        
+    }
+    
+    if(number2 %div > 1 ){
+      arr.push(number2)
+    }
+   
+}
+console.log(arr);
+
+// Question 12 :---
+// integer
+// var a = Number(prompt('input a'));
+var a = 3
+// var b = Number(prompt('input b'));
+var b = 4
+// var c = Number(prompt('input c'));
+var c = 5
+
+let max = a;
+
+if(b >= max){
+    max = b;
+}else if(c >= max){
+    max = c;
+}
+
+if(max === a){
+    console.log((b * b + c * c)===( a * a ))
+}else if(max === b){
+    console.log((a * a + c * c)===( b * b ))
+    
+}else if(max === c){
+    console.log((b * b + a * a)===( c * c ))
+
+}
+
+// Question 13 :-- Benjamin bulbs
+
+var number2 = Number(prompt('number of bulb'));
+
+for(let i=1; i*i<=number2; i++){
+    console.log(`bulb that will on next fluctuation is ${i*i}`);
+}
+
+
+
